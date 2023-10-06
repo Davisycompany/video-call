8b9813458c7663a6d2de60fbdd8fff48f4f99f37
git-fbi <89396036+Theanishtar@users.noreply.github.com>
2023-10-06 14:59:41 +0700

    config host

--- src/main/java/com/davis/config/WebSocketConfig.java
+++ src/main/java/com/davis/config/WebSocketConfig.java
@@ -20,8 +20,9 @@ public class WebSocketConfig {
 		com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
 		config.setHostname(host);
 		config.setPort(port);
-		config.setOrigin("https://davisycompany.github.io");
-		config.setAllowCustomRequests(true);
-		return new SocketIOServer(config);
+
+		SocketIOServer server = new SocketIOServer(config);
+		
+		return server;
 	}
 }
