cbd0014baa9fd15cab94f7e4d60609260460e190
git-fbi <89396036+Theanishtar@users.noreply.github.com>
2023-10-06 13:59:36 +0700

    config cros

--- src/main/java/com/davis/config/WebSocketConfig.java
+++ src/main/java/com/davis/config/WebSocketConfig.java
@@ -1,33 +1,29 @@
 package com.davis.config;
 
-
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
+import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
 
 @Configuration
 public class WebSocketConfig {
 
-  @Value("${socket.host}")
-  private String host;
+	@Value("${socket.host}")
+	private String host;
 
-  @Value("${socket.port}")
-  private int port;
+	@Value("${socket.port}")
+	private int port;
 
-  @Bean
-  public SocketIOServer socketIOServer() throws Exception {
-    com.corundumstudio.socketio.Configuration config =
-        new com.corundumstudio.socketio.Configuration();
-    config.setHostname(host);
-    config.setPort(port);
-    config.setOrigin("http://127.0.0.1:5500");
-    config.setAllowHeaders("Content-Type");
-    config.setAllowHeaders("Authorization");
-    config.setAllowCustomRequests(true);
-    return new SocketIOServer(config);
-  }
+	@Bean
+	public SocketIOServer socketIOServer() throws Exception {
+		com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+		config.setHostname(host);
+		config.setPort(port);
+		config.setOrigin("*");
+		config.setAllowCustomRequests(true);
+		return new SocketIOServer(config);
+	}
 }
